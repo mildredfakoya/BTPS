ed9c1cf700aad2599d2caf0ae14b32dea6d2627c
Mildred Fakoya <luvlyayee@gmail.com>
2020-04-23 13:46:28 -0400

    update of home page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'helper.php';
 $user_helper = new helper();
 ?>
 <div class ="jumbotron">
-<p>We welcome you to BONNE TERRE PREPARATORY SCHOOL LTD which was established in September 1992. This school provides education for children of ages 2 1/2 years to 16 years.<br/>
+<p style = "font-family:courier,arial,helvetica;">We welcome you to BONNE TERRE PREPARATORY SCHOOL LTD which was established in September 1992. This school provides education for children of ages 2 1/2 years to 16 years.<br/>
 Students of ages 2 1/2 to 4 attend Pre- Kindergarten. The elementary section is from Kindergarten (4 turning 5 no later than February) to Grade 6.<br/>
 The high school section is from 11+ years old. Our high school which commenced Sept. 2007 presently comprises Grades 7 to 11 (Forms 1 – 5).</br>
 Students of Grade 2 & Grade 4 write the Minimum Standards Test set by the Ministry of Education. Grade 6 students write the Common Entrance Examination for entry into secondary school. High school students will take the CSEC Examinations.
@@ -14,49 +14,39 @@ Students of Grade 2 & Grade 4 write the Minimum Standards Test set by the Minist
 
 <div class ='outer'>
 <div class ='heading'><h3 style='color:#0000ff'>Third term 2020 Welcome address</h3></div>
-<div class ='container'><video width="1100" height="500" controls autoplay>
+<div class ='container'><video width="1100" height="500" controls>
   <source src="images/welcome.mp4" type="video/mp4">
   Your browser does not support HTML5 video.
   </video>
 </div></div>
+<p style = "font-family:courier,arial,helvetica;"> We are working round the clock to ensure that our students get the best and remain the best. See below samples recorded classes. These will be available as a supplement to the live classes.
+</p>
 
-<div id="demo" class="carousel slide outer" data-ride="carousel">
-<div class ='heading'><h3 style='color:#0000ff'>Image Gallery</h3></div>
-  <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
+<div class ='outer'>
+<div class ='heading'><h3 style='color:#0000ff'>Lecture Highlight</h3></div>
+<div class ='container'><video width="1100" height="500" controls>
+  <source src="images/ss.mp4" type="video/mp4">
+  Your browser does not support HTML5 video.
+  </video>
+</div>
+<div class ='container'><video width="1100" height="500" controls>
+  <source src="images/maths.mp4" type="video/mp4">
+  Your browser does not support HTML5 video.
+  </video>
+</div>
 
-  <!-- The slideshow -->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="images/image1.jpg" alt="image1" width="1100" height="500">
-    </div>
-    <div class="carousel-item">
-      <img src="images/image2.jpg" alt="image2" width="1100" height="500">
-    </div>
-    <div class="carousel-item">
-      <img src="images/image3.jpg" alt="image3" width="1100" height="500">
-    </div>
-    <div class="carousel-item">
-      <img src="images/image4.jpg" alt="image4" width="1100" height="500">
-    </div>
-    <div class="carousel-item">
-      <img src="images/image4.jpg" alt="image4" width="1100" height="500">
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
+<div class ='container'><video width="1100" height="500" controls>
+  <source src="images/alana.mp4" type="video/mp4">
+  Your browser does not support HTML5 video.
+  </video>
+</div>
 
 </div>
+
+
+
+
+
 </div>
 </body>
 </html>
