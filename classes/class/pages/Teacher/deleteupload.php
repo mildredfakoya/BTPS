bd9a0af167c7b3d94cfcf46b9324d3e1a8e7883a
Mildred Fakoya <luvlyayee@gmail.com>
2020-04-23 17:17:25 -0400

    correcting delete uploads

--- classes/class/pages/Teacher/deleteupload.php
+++ classes/class/pages/Teacher/deleteupload.php
@@ -5,7 +5,7 @@ try{
   # initialize a serial number counter that will increment after every iteration
   $counter = 0;
   # query the sites table for the list of available sites
-  $sqldistrict = "SELECT * FROM ihs_video_uploads where created_by_firstname = $firstname and created_by_lastname = $lastname ORDER BY created_at";
+  $sqldistrict = "SELECT * FROM ihs_video_uploads where created_by_firstname = '$firstname' and created_by_lastname = '$lastname' ORDER BY created_at";
   $stmtdistrict = $user_home->runQuery($sqldistrict);
   $stmtdistrict->execute();
 
