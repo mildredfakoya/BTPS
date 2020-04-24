58c421d73b7d62ef1f3d046692ab0d0682aaa7b2
Mildred Fakoya <luvlyayee@gmail.com>
2020-04-24 03:33:49 -0400

    adding copyright

--- classes/class/pages/Student/includes/studentfooter.php
+++ classes/class/pages/Student/includes/studentfooter.php
@@ -1,3 +1,2 @@
-<?php echo "<p>Copyright &copy; 2020 -" . date("Y") . "Mildred Fakoya for Bonne Terre Preparatory School</p>";
 </body>
 </html>
--- classes/class/pages/Student/studenthome.php
+++ classes/class/pages/Student/studenthome.php
@@ -9,7 +9,16 @@ require_once "includes/studentheader.php";
     </div>
 	<div class ='container'>
 		<p>We welcome you to our online learning platform. we will provide you all the support that you need to help you get settled.<br/>
-			We look forward to best results from you and a good association of studies with you.”</p>
+			We look forward to best results from you and a good association of studies with you.</p>
+			<p>For best user experience, please use the following browsers: Listed in order of best experience
+			<ol>
+       <li>Google Chrome</li>
+			 <li>Apple Safari</li>
+			 <li>Opera</li>
+			 <li>Moxilla Firefox</li>
+			 <li>Internet Explorer (edge)</li>
+			</ol>
+		</p>
     </div>
 </div>
 
