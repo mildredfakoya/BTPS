f6953bfb8c776e562cdf1abdbade5c9f9aaa720d
Mildred Fakoya <luvlyayee@gmail.com>
2020-09-06 16:29:10 -0400

    completion of chat room

---
+++ classes/class/pages/Teacher/responsechat9.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'includes/teacherinit.php';
+require_once '../../../../aes.php';
+$inputkey = "marketdayanyigba";
+$blocksize = 256;
+
+date_default_timezone_set('America/dominica');
+$createdat = date("y-m-d h:i:sa");
+$createdbyemail = $row['email'];
+$chatid= !empty($_POST['chatidres']) ? $helper->test_input($_POST['chatidres']) : null;
+$content= !empty($_POST['respond']) ? $helper->test_input($_POST['respond']) : null;
+$recipientmail= !empty($_POST['recipientres']) ? $helper->test_input($_POST['recipientres']) : null;
+
+
+
+try{
+ $sqlf="INSERT INTO chats9(created_at, created_by_email, chat_id,  content, recipient_mail)
+ VALUES(:created_at, :created_by_email, :chat_id,  :content, :recipient_mail)";
+ $stmtf = $user_home->runQuery($sqlf);
+ $stmtf->bindValue(':created_at', $createdat);
+ $stmtf->bindValue(':created_by_email', $createdbyemail);
+ $stmtf->bindValue(':chat_id', $chatid);
+ $stmtf->bindValue(':content', $content);
+ $stmtf->bindValue(':recipient_mail', $recipientmail);
+ $resultf = $stmtf->execute();
+
+  if($resultf){
+		echo "Chat has been created";
+	   }
+	   else{
+		echo "Chat was not created";
+	   }
+}
+
+ catch(PDOException $e)
+    {
+      //die("SYSTEM FAILURE!! Please contact your administrator");
+   echo $e->getMessage();
+    }
+
+
+
+?>
--- classes/class/pages/Teacher/teacherhome.php
+++ classes/class/pages/Teacher/teacherhome.php
@@ -83,6 +83,7 @@ require_once 'includes/teachernav.php';
                       <a class="nav-link dropdown-toggle" style ="color:White; font-size:large" href="#" id="navbardrop" data-toggle="dropdown">Pre K Virtual Classroom</a>
                       <div class="dropdown-menu">
                       <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=tXyaRrp9qHhYKUVuZstx7emxPJM%3D&courseId=_905235_1&timestamp=1599231140&inviteId=BB%253FBB_23LzHX6bGZvGouI2M9yB%2Fb3TNIRK8jYPhrDVplLtgrMtUG%252B4ivX5IA%253D%253D">Pre K</a>
+                      <a class="dropdown-item" target = "_blank" href="chatroompk.php">Chat Room and Discussion Forum</a>
                     </div>
                     </li>
 
@@ -107,6 +108,7 @@ require_once 'includes/teachernav.php';
                   <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=NU4egjD0ManOxrHwgd8Rw%2BNalRU%3D&courseId=_905237_1&timestamp=1599229806&inviteId=BB%253FBB_0RAaBJIQQ9cZeJeIvMigcoU5VZzl%252BSKldJ3B%252BnU9e84tUG%252B4ivX5IA%253D%253D">Grade 1 - Blackboard</a>
                   <!--<a class="dropdown-item" target= "_blank" href="https://us02web.zoom.us/j/87114057766">Start Grade 1 - Zoom Class</a>-->
+                  <a class="dropdown-item" target = "_blank" href="chatroom1.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -118,6 +120,7 @@ require_once 'includes/teachernav.php';
                     <div class="dropdown-menu">
                       <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=wiwJGJ%2BewzackZcJ71FkU7N2CNc%3D&courseId=_797059_1&timestamp=1599227347&inviteId=BB%253FBB_qs%2FmKAWONPeBMazi65eANgW6Oy0el%2FQxL2ForN8720stUG%252B4ivX5IA%253D%253D">Grade 2 - Blackboard</a>
                       <!--<a class="dropdown-item" target= "_blank" href="https://us02web.zoom.us/j/82052708190">Start Grade 2 - Zoom Class</a>-->
+                      <a class="dropdown-item" target = "_blank" href="chatroom2.php">Chat Room and Discussion Forum</a>
                     </div>
                   </li>
 
@@ -128,6 +131,7 @@ require_once 'includes/teachernav.php';
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=YzRchIeL9LYJNkn50jU3eQ%2FsoeE%3D&courseId=_797060_1&timestamp=1599227762&inviteId=BB%253FBB_j2VFeZQam2t38ArLwXTnrsUno9zTQpePlLtwrRW%252B1JUtUG%252B4ivX5IA%253D%253D">Grade 3 - Blackboard</a>
                   <!--<a class="dropdown-item" target= "_blank" href="https://us02web.zoom.us/j/84702993730">Start Grade 3 - Zoom Class</a>-->
+                  <a class="dropdown-item" target = "_blank" href="chatroom3.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -138,6 +142,7 @@ require_once 'includes/teachernav.php';
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=FCpxuwP7o8rxmAt90pGzXl%2BIzTI%3D&courseId=_797061_1&timestamp=1599227950&inviteId=BB%253FBB_i%252Bx55KLOcLz8exwnj%252Bo9J0%252BdYsh30cWNzcKBa3gF17otUG%252B4ivX5IA%253D%253D">Grade 4 - Blackboard</a>
                   <!--<a class="dropdown-item" target= "_blank"  href="https://us02web.zoom.us/j/85675057132">Start Grade 4 - Zoom Class</a>-->
+                  <a class="dropdown-item" target = "_blank" href="chatroom4.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -148,6 +153,7 @@ require_once 'includes/teachernav.php';
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=hF2dujsBtLpsjKDoNJMG94vLMDU%3D&courseId=_797062_1&timestamp=1599228038&inviteId=BB%253FBB_iPtmJpdgcUp%252B2gDLjyJHxkakoIZf1adpj4%2FRY11sat0tUG%252B4ivX5IA%253D%253D">Grade 5 - Blackboard</a>
                   <!--<a class="dropdown-item" target= "_blank" href="https://us02web.zoom.us/j/87063460642">Start Grade 5 - Zoom Class</a>-->
+                  <a class="dropdown-item" target = "_blank" href="chatroom5.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -158,6 +164,7 @@ require_once 'includes/teachernav.php';
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=HaLL9rFxej53%2BJuCSzmcpouMJtU%3D&courseId=_797063_1&timestamp=1599228104&inviteId=BB%253FBB_09GKL%252Bd22N3zKG%252B4BUJGwgn0u%252BrwRIalOsMok82obTMtUG%252B4ivX5IA%253D%253D">Grade 6 - Blackboard</a>
                   <!--<a class="dropdown-item" target= "_blank" href="https://us02web.zoom.us/j/81104844111">Start Grade 6 - Zoom Class</a>-->
+                  <a class="dropdown-item" target = "_blank" href="chatroom6.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -167,6 +174,7 @@ require_once 'includes/teachernav.php';
                 <a class="nav-link dropdown-toggle" style ="color:White; font-size:large" href="#" id="navbardrop" data-toggle="dropdown">Grade 7 Virtual Classroom</a>
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=4KLitDVjZFFqpjBxu2taMXJwZtc%3D&courseId=_797064_1&timestamp=1599228160&inviteId=BB%253FBB_66zgpPwVbYAGfysBAkDJVsL%252Bf6yVBqOonYfJtJ0h1ogtUG%252B4ivX5IA%253D%253D">Grade 7</a>
+                  <a class="dropdown-item" target = "_blank" href="chatroom7.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -177,6 +185,7 @@ require_once 'includes/teachernav.php';
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=Xe1Ib6JEJsYkJvtKP4uDI8FQs6Y%3D&courseId=_797058_1&timestamp=1599228208&inviteId=BB%253FBB_HPQJJtzWXp20IDajHI6fzytwJp0ymsQ2JDoPMhXF0A4tUG%252B4ivX5IA%253D%253D">Grade 8 - Blackboard</a>
                   <!--<a class="dropdown-item" target= "_blank" href=" https://us02web.zoom.us/j/85617723160">Start Grade 8 - Zoom Class</a>-->
+                  <a class="dropdown-item" target = "_blank" href="chatroom8.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -186,6 +195,7 @@ require_once 'includes/teachernav.php';
                 <a class="nav-link dropdown-toggle" style ="color:White; font-size:large" href="#" id="navbardrop" data-toggle="dropdown">Grade 9 Virtual Classroom</a>
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=IjBdie3ajDWUoK3GgMMyjPH8nio%3D&courseId=_797065_1&timestamp=1599228430&inviteId=BB%253FBB_AVGF2L7X4bl1uzatbqma6I0KqULHKY9OuwXZOrDwTBotUG%252B4ivX5IA%253D%253D">Grade 9</a>
+                  <a class="dropdown-item" target = "_blank" href="chatroom9.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -195,6 +205,7 @@ require_once 'includes/teachernav.php';
                 <a class="nav-link dropdown-toggle" style ="color:White; font-size:large" href="#" id="navbardrop" data-toggle="dropdown">Grade 10 Virtual Classroom</a>
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=bmf4CT1H1ooaL0wHGJToeULmHEg%3D&courseId=_797066_1&timestamp=1599228506&inviteId=BB%253FBB_Xt7yALYIvwFt%2FZnwrvShEGPXSVFEDI2JM2TUWKHIkQAtUG%252B4ivX5IA%253D%253D">Grade 10</a>
+                  <a class="dropdown-item" target = "_blank" href="chatroom10.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
@@ -204,6 +215,7 @@ require_once 'includes/teachernav.php';
                 <a class="nav-link dropdown-toggle" style ="color:White; font-size:large" href="#" id="navbardrop" data-toggle="dropdown">Grade 11 Virtual Classroom</a>
                 <div class="dropdown-menu">
                   <a class="dropdown-item" target = "_blank" href="http://www.blackboard.com/coursesites/?sig=mwLIcO5lsH3W5ViAIbYE3KUC%2F%2FQ%3D&courseId=_797067_1&timestamp=1599228566&inviteId=BB%253FBB_Kx%252Bi70DERUZ2ompKgmm0avSbWcR0s5IuJ6VF8u%252ByM7MtUG%252B4ivX5IA%253D%253D">Grade 11</a>
+                  <a class="dropdown-item" target = "_blank" href="chatroom11.php">Chat Room and Discussion Forum</a>
                 </div>
                 </li>
 
